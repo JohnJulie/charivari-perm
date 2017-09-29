3b4182f00a5d8a6b6866063a6c1138ed251206b8
Julie PELLETIER <julie.pelletier@ineat-conseil.fr>
2017-09-29 13:39:44 +0200

    feat(back): add hibernate + spring config

---
+++ src/main/java/com/adi3000/charivariperm/api/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package com.adi3000.charivariperm.api.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.adi3000.charivariperm.api")
+public class AppConfig {
+
+}
